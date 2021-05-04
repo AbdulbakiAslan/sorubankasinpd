cd42ce12f5fe8dd76d3eb577c2b9d56a372a8105
AbdulbakiAslan <abdulbaki.aslan@ogr.dpu.edu.tr>
2021-05-04 21:49:50 +0300

    feat: Add button

---
+++ src/SoruBankasi/SoruBankasi.java
@@ -0,0 +1,86 @@
+package SoruBankasi;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SoruBankasi extends JFrame {
+    private Container container;
+    private JMenuBar menuBar;
+    private JMenuItem menuSoruEkle;
+    private JMenuItem menuSoruSil;
+    private JMenuItem menulistele;
+    private JMenuItem menuSoruAra;
+    private JMenuItem anaSayfa;
+    private JComboBox siralamaTuru;
+    private JButton secButton;
+    private JPanel anaPanel;
+    private JPanel yeniPanel;
+
+    private JLabel soruMetni;
+    private JLabel soruCevabi;
+    private JLabel soruPuani;
+    private JLabel soruTuru;
+
+
+    private JTextArea hoşgeldiniz;
+    private JTable tablo;
+
+
+    public SoruBankasi() {
+        super("Soru Bankası");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(700, 500);
+        Toolkit toolkit = getToolkit();
+        Dimension size = toolkit.getScreenSize();
+        this.setLocation(size.width / 2 - getWidth() / 2, size.height / 2 - getHeight() / 2);
+        menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        anaSayfa = new JMenuItem("AnaSayfa");
+
+        menuBar.add(anaSayfa);
+
+        anaSayfa.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //anasayfa();
+            }
+        });
+
+        menuSoruEkle = new JMenuItem("Yeni Soru");
+        menuBar.add(menuSoruEkle);
+        menuSoruEkle.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  yeniSoruMenusu();
+            }
+        });
+        menuSoruSil = new JMenuItem("Soru Sil");
+        menuBar.add(menuSoruSil);
+        menuSoruSil.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  SoruSil();
+            }
+        });
+        menulistele = new JMenuItem("Listele");
+        menuBar.add(menulistele);
+        menulistele.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  yeniSoruListeleme();
+            }
+        });
+        menuSoruAra = new JMenuItem("Soru Ara");
+        menuBar.add(menuSoruAra);
+        menuSoruAra.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               // yeniSoruAra();
+            }
+        });
+    }
+        public static void main (String[]args){
+            SoruBankasi banka = new SoruBankasi();
+            banka.setVisible(true);
+
+        }
+    }
+
